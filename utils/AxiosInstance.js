7f5976700c5ec0656a93d00a8a5961778bd71375
ganesh-basavoju <basavojuganesh@gmail.com>
2025-06-02 12:57:28 +0530

    fgf

--- utils/AxiosInstance.js
+++ utils/AxiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL:  "https://zendor-backend.onrender.com", //"https://zendor-backend.onrender.com/api" 
+  baseURL:  "https://zendor-backend.onrender.com/api", //"https://zendor-backend.onrender.com/api" 
   headers: {
     "Content-Type": "application/json",
   },
