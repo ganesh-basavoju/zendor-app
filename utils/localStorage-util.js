2704e2962909af003961fadf1e6808f2b4e3687d
lalith <lalithdev123@gmail.com>
2025-06-02 10:23:51 +0530

    latest-2

--- utils/localStorage-util.js
+++ utils/localStorage-util.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 
 export const saveCartToLocalStorage = (cartState) => {
   try {
@@ -22,28 +21,3 @@ export function loadCartFromLocalStorage() {
     return { items: [], totalAmount: 0, isGuest: false };
   }
 }
-=======
-export const loadCartFromLocalStorage = () => {
-  if (typeof window === 'undefined') {
-    // Running on server, return empty cart
-    return { items: [] };
-  }
-  try {
-    const serializedState = localStorage.getItem('cart');
-    return serializedState ? JSON.parse(serializedState) : { items: [] };
-  } catch (e) {
-    console.warn("Could not load cart from localStorage", e);
-    return { items: [] };
-  }
-};
-  
-  export const saveCartToLocalStorage = (cartState) => {
-    try {
-      const serializedState = JSON.stringify(cartState);
-      localStorage.setItem('cart', serializedState);
-    } catch (e) {
-      console.warn("Could not save cart to localStorage", e);
-    }
-  };
-  
->>>>>>> aa1b56f678b0f3b1dd4ea8213eac21d1eac77328
