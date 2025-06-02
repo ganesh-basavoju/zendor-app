577fd83a394ec91278e1e1165219524e9b3f0571
lalith <lalithdev123@gmail.com>
2025-06-02 08:58:15 +0530

    changed local storage access

--- app/category/acoustics/page.jsx
+++ app/category/acoustics/page.jsx
@@ -1,15 +1,11 @@
 "use client";
-import { HeroSection } from "@/components/acoustics/HeroSection";
+
 import { JourneySection } from "@/components/acoustics/JourneySection";
-import { CommercialSolutionsSection } from "@/components/acoustics/CommercialSolutionsSection";
 import { ApplicationsSection } from "@/components/acoustics/ApplicationsSection";
 import { ClientsSection } from "@/components/acoustics/ClientsSection";
-import { ContactSection } from "@/components/acoustics/ContactSection";
 import SustainableMaterialsSection from "@/components/acoustics/SustainableMaterialsSection";
 import AdvancedTechnologySection from "@/components/acoustics/AdvancedTechnologySection";
 import CustomSolutionsSection from "@/components/acoustics/CustomSolutionsSection";
-import { FeaturedAcousticSection } from "@/components/acoustics/FeaturedAcousticSection";
-import { SoundDiffusionSection } from "@/components/acoustics/SoundDiffusionSection";
 import ContactFormSection from "@/components/acoustics/ContactFormSection";
 import Link from "next/link";
 import Image from "next/image";
