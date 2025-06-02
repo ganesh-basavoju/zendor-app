687d2ad4613f4656dceb91b3358c5b66a9529489
lalith <lalithdev123@gmail.com>
2025-06-02 10:22:53 +0530

    latest-2

--- utils/AxiosInstance.js
+++ utils/AxiosInstance.js
@@ -1,11 +1,8 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-<<<<<<< HEAD
-  baseURL:  "https://zendor-backend-s1ci.onrender.com/api", //"https://zendor-backend.onrender.com/api" 
-=======
+  // baseURL:  "https://zendor-backend-s1ci.onrender.com/api", //"https://zendor-backend.onrender.com/api" 
   baseURL:  "https://zendor-backend.onrender.com/api",  //"http://localhost:5000/api", ], 
->>>>>>> aa1b56f678b0f3b1dd4ea8213eac21d1eac77328
   headers: {
     "Content-Type": "application/json",
   },
